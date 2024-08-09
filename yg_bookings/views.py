3571dc21e3be17af75f484cfb2feda05cb143e27
Tanika Irwin <tanika-i@outlook.com>
2024-08-09 14:57:29 +0000

    Creating a view for creating a new booking

--- yg_bookings/views.py
+++ yg_bookings/views.py
@@ -14,7 +14,7 @@ class SignUpView(CreateView):
     """
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    template_name = 'registration/index.html'
 
 @method_decorator(login_required, name='dispatch')
 class BookingListView(ListView):
@@ -22,11 +22,23 @@ class BookingListView(ListView):
     This view lists all bookings made by the logged-in user.
     """
     model = Booking
-    template_name = 'bookings/booking_list.html'
+    template_name = 'bookings/accountpage.html'
 
     def get_queryset(self):
         return Booking.objects.filter(user=self.request.user)
 
+@method_decorator(login_required, name='dispatch')
+class BookingCreateView(CreateView):
+    """ 
+    This view handles the creation of a new booking.
+    """
+    form_class = BookingForm
+    success_url = reverse_lazy('booking_list')
+    template_name = 'bookings/booking_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class HomePage(TemplateView):
     """
