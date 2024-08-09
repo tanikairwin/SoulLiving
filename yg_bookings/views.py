af493260338ecf56b172acb1fb9745a5aaf3c76e
Tanika Irwin <tanika-i@outlook.com>
2024-08-09 14:47:33 +0000

    Importing required classes into views.py

--- yg_bookings/views.py
+++ yg_bookings/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView
+from .forms import CustomUserCreationForm, BookingForm
+from .models import YogaClass, Booking
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
