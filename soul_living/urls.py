ff11ad7d069ae004200183ecf4e8e5505669b227
Tanika Irwin <tanika-i@outlook.com>
2024-08-09 16:08:08 +0000

    Adding url patterns to both urls.py files

--- soul_living/urls.py
+++ soul_living/urls.py
@@ -18,8 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
 
+
+urlpatterns = [
+
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
+]
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='home/', permanent=False), name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('home/',include('yg_bookings.urls')),
 ]
--- yg_bookings/urls.py
+++ yg_bookings/urls.py
@@ -1,5 +1,12 @@
 from yg_bookings import views
 from django.urls import path
+from .views import SignUpView, BookingListView, BookingCreateView
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('useraccount/', BookingListView.as_view(), name='booking_list'),
+    path('bookings/new/', BookingCreateView.as_view(), name='booking_create'),
+]
 
 urlpatterns = [
     path('',views.HomePage.as_view(), name='home'),
