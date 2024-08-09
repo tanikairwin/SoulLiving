21df3b788afb69ff8e9c821521b810e8692f7e39
Tanika Irwin <tanika-i@outlook.com>
2024-08-09 17:38:56 +0000

    Adding index.html to subdirectory and changing its content

--- yg_bookings/views.py
+++ yg_bookings/views.py
@@ -14,7 +14,7 @@ class SignUpView(CreateView):
     """
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/index.html'
+    template_name = 'SoulLiving/index.html'
 
 @method_decorator(login_required, name='dispatch')
 class BookingListView(ListView):
@@ -22,7 +22,7 @@ class BookingListView(ListView):
     This view lists all bookings made by the logged-in user.
     """
     model = Booking
-    template_name = 'bookings/accountpage.html'
+    template_name = 'SoulLiving/accountpage.html'
 
     def get_queryset(self):
         return Booking.objects.filter(user=self.request.user)
@@ -44,28 +44,4 @@ class HomePage(TemplateView):
     """
     Displays home page
     """
-    template_name = 'index.html'
-
-class BookingSchedulePage(TemplateView):
-    """
-    Displays booking scheduling page
-    """
-    template_name = 'bookings.html'
-
-class LoginPage(TemplateView):
-    """
-    Displays user login page
-    """
-    template_name = 'login.html'
-
-class SessionRegistryPage(TemplateView):
-    """
-    Displays session registry page
-    """
-    template_name = 'completebookings.html'
-
-class UserProfilePage(TemplateView):
-    """
-    Displays users accounts page when logged in
-    """
-    template_name = 'accountpage.html'
+    template_name = 'SoulLiving/index.html'
