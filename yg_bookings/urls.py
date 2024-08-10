b15ec6d7e4d36788e213cb5fff771cbab2acb0bf
Tanika Irwin <tanika-i@outlook.com>
2024-08-10 12:22:02 +0000

    Solving migrations issue

--- yg_bookings/urls.py
+++ yg_bookings/urls.py
@@ -10,9 +10,5 @@ urlpatterns = [
 
 urlpatterns = [
     path('',views.HomePage.as_view(), name='home'),
-    path("bookings/",views.BookingSchedulePage.as_view(), name='bookings'),
-    path("completebookings/",views.SessionRegistryPage.as_view(), name='completebookings'),
-    path("login/",views.LoginPage.as_view(), name='login'),
-    path("useraccount/",views.UserProfilePage.as_view(), name='useraccount')
     
 ]
