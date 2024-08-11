80f9efa768d3fd2ed5b3d2c015b1e8c777337233
Tanika Irwin <tanika-i@outlook.com>
2024-08-11 00:24:54 +0000

    Updating urls that includes new view to JSON data

--- yg_bookings/urls.py
+++ yg_bookings/urls.py
@@ -1,6 +1,6 @@
 from yg_bookings import views
 from django.urls import path
-from .views import SignUpView, BookingListView, BookingCreateView, BookView
+from .views import SignUpView, BookingListView, BookingCreateView, BookView, BookingJSONView
 
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
     path('bookings/new/', BookingCreateView.as_view(), name='booking_create'),
     path('bookings/', BookingListView.as_view(), name='bookings'),
     path('book/<int:pk>/', BookView.as_view(), name='book'),
+    path('bookings/json/', BookingJSONView.as_view(), name='booking_json'),
 ]
 
