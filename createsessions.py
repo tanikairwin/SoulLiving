d0a96ab57f8c9a5aeca8da27e10dfd9d4f68775b
Tanika Irwin <tanika-i@outlook.com>
2024-08-12 11:15:11 +0000

    Updating createsessions with the correct date and times

--- createsessions.py
+++ createsessions.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 import pytz
 
 # Setup Django environment
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SoulLiving/.settings')
 django.setup()
 
 from yg_bookings.models import Sessions
@@ -26,17 +26,17 @@ def create_sessions():
 
     for single_date in (start_date + timedelta(n) for n in range((end_date - start_date).days)):
         if single_date.strftime('%A') in days_of_week:
-            for start_time, duration in session_times:
+            for time, duration in session_times:
                 start_datetime = timezone.localize(datetime.combine(single_date, datetime.strptime(start_time, '%H:%M').time()))
                 end_datetime = start_datetime + timedelta(minutes=duration)
 
-                title = f"{'Meditation' if start_time == '19:00' else 'Yoga'} {'and Yoga' if start_time == '12:00' else ''}"
+                title = f"{'Meditation' if time == '19:00' else 'Yoga'} {'and Yoga' if time == '12:00' else ''}"
                 
                 Sessions.objects.create(
                     title=title,
-                    start_time=start_datetime,
+                    time=start_datetime,
                     end_time=end_datetime,
-                    description=f"{title} session on {single_date.strftime('%A')} at {start_time}",
+                    description=f"{title} session on {single_date.strftime('%A')} at {time}",
                     location="Yoga Studio"
                 )
 
