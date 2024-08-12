f3b4191239a2ebff9bc720d304bd50aee984d666
Tanika Irwin <tanika-i@outlook.com>
2024-08-12 22:49:01 +0000

    Adding profile and login out view urls

--- yg_bookings/urls.py
+++ yg_bookings/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import (
     HomePage, 
-    SignUpView, 
-    BookingListView, 
+    SignUpView,  
     BookingView, 
     BookingConfirm, 
     LoginView, 
@@ -12,11 +12,13 @@ from .views import (
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('useraccount/', BookingListView.as_view(), name='booking_list'),
+    # path('useraccount/', BookingListView.as_view(), name='booking_list'),
     path('bookings/', BookingView.as_view(), name='bookings'),
     path('sessions/', BookingView.as_view(), name='sessions'),
     path('confirm-booking/<int:session_id>/', BookingConfirm.as_view(), name='confirm_booking'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('accountpage/', ProfileView.as_view(), name='profile'),
     path('bookings/json/', BookingJSONView.as_view(), name='booking_json'),
 ]
 
