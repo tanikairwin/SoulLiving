1238265337d0723f84f73a1418a6490f47e671fd
Tanika Irwin <tanika-i@outlook.com>
2024-08-13 23:01:54 +0000

    Reverting back to djangos LogoutView due to Internal Server Error with import logging

--- yg_bookings/urls.py
+++ yg_bookings/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 from .views import (
     HomePage,
     CustomLoginView,
@@ -9,7 +10,7 @@ urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('register/', views.register_user, name='register'),
     path('accounts/login/', CustomLoginView.as_view(), name='login'),
-    path('accounts/logout/', custom_logout_view, name='logout'),
+    path('accounts/logout/', LogoutView(next_page='home'), name='logout'),
 ]
 
 
