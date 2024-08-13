7aa84dd2c45101d76f0566ebf9e8f76ad05fcf8d
Tanika Irwin <tanika-i@outlook.com>
2024-08-13 14:17:19 +0000

    Changes to urls to attempt to redirect register page to login and login to account page

--- yg_bookings/urls.py
+++ yg_bookings/urls.py
@@ -5,20 +5,23 @@ from .views import (
     HomePage, 
     LoginView, 
     ProfileView,
-    register_user
+    register_user,
+    # registration_success_view
 )
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('register/', views.register_user, name='register'),
+     path('register/login/', LoginView.as_view(), name='login'),
+    # path('register/success/', views.registration_success_view, name='registration_success'),
     # path('signup/', SignUpView.as_view(), name='signup'),
     # path('useraccount/', BookingListView.as_view(), name='booking_list'),
     # path('bookings/', BookingView.as_view(), name='bookings'),
     # path('sessions/', BookingView.as_view(), name='sessions'),
     # path('confirm-booking/<int:session_id>/', BookingConfirm.as_view(), name='confirm_booking'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('accountpage/', ProfileView.as_view(), name='profile'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/accountpage/', ProfileView.as_view(), name='profile'),
     # path('bookings/json/', BookingJSONView.as_view(), name='booking_json'),
 ]
 
